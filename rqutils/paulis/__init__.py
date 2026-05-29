8c49c0548777d3b30e813fb638bb36a709f5a6ba
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2026-05-29 09:48:55 +0900

    Adding __init__

---
+++ rqutils/paulis/__init__.py
@@ -0,0 +1,21 @@
+"""
+============================================================================================
+Pauli matrix utilities (:mod:`rqutils.paulis`)
+============================================================================================
+
+.. module:: rqutils.paulis
+
+Generalized Pauli matrices
+==========================
+
+.. autosummary::
+   :toctree: ../generated
+
+Symplectic representation of Pauli sums
+=======================================
+
+.. autosummary::
+   :toctree: ../generated
+"""
+# For backward compatibility
+from rqutils.paulis.general import paulis
